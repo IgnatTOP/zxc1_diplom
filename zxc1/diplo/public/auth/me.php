c494b022fee48b3413040e1c91978c23df2a0f84
IgnatZorin <i@ignatz.ru>
2026-03-02 19:28:32 +0300

    feat: Introduce interactive inline keyboards for Telegram support notifications and remove webhook-related configurations.

--- zxc1/diplo/public/auth/me.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../../includes/init.php';
-
-json_ok(['user' => current_user()]);
-
--- zxc1/zxc1/diplo/includes/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// Common bootstrap for all endpoints
-session_start();
-require_once __DIR__ . '/functions.php';
-set_security_headers();
-
